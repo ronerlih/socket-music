8c8cf119b9d0888a54c1501cf958f821ddaf7546
ron erlih <ronerlih@gmail.com>
2020-06-27 00:18:07 -0400

    init

--- client/src/utils/tone.js
+++ client/src/utils/tone.js
@@ -1,9 +1,9 @@
 import { Synth } from "tone";
 //create a synth and connect it to the master output (your speakers)
-const synth = new Synth().toMaster();
 // const sounds = [280, 320, 350,370,400, 470, 520,540, 560,590,610,640]
 
 export default {
     play: yPosition => {
+        const synth = new Synth().toMaster();
         synth.triggerAttackRelease((yPosition), "8n")}
 };
